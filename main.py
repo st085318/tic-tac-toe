26e8e8fe5853af06f593e8ebc17ae5fa4a590387
st085318 <st085318@student.spbu.ru>
2020-10-11 00:22:58 +0300

    create exceptions and function whose turn

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ from random import *
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class PlaceOfMoveException(Exception):
+    def __init__(self):
+        self.txt = "ERROR You make turn into occupied field"
 
-#TODO: make exeption
+
+class SkippedEndException(Exception):
+    def __init__(self):
+        self.txt = "ERROR You 2 players are winners, so you skip end of the game"
+
+
 class TicTacToe(object):
     field = []
 
@@ -27,11 +35,17 @@ class TicTacToe(object):
 
     def make_turn(self, x_coordinate, y_coordinate):
         try:
+            x_coordinate -= 1
+            y_coordinate -= 1
             if self.field[y_coordinate][x_coordinate] == -1:
                 self.field[y_coordinate][x_coordinate] = self.current_player
                 self.current_player = (1+self.current_player) % 2
-        except BaseException:
-            print('exc')
+            else:
+                raise PlaceOfMoveException()
+        except IndexError:
+            print('x and y must be between 1 and 3')
+        except PlaceOfMoveException as pme:
+            print(pme.txt)
 
     def is_win(self):
         cross_win = False
@@ -57,40 +71,48 @@ class TicTacToe(object):
                 cross_win = True
             if self.field[1][1] == 0:
                 circle_win = True
-        if not cross_win and not circle_win:
-            self.winner = "Nobody"
-        elif cross_win and circle_win:
-            self.winner = "ERROR"
-        elif circle_win:
-            self.winner = self.circle_name
-        elif cross_win:
-            self.winner = self.cross_name
-        print(self.winner)
+        try:
+            if not cross_win and not circle_win:
+                self.winner = "Nobody"
+            elif cross_win and circle_win:
+                raise SkippedEndException()
+            elif circle_win:
+                self.winner = self.circle_name
+            elif cross_win:
+                self.winner = self.cross_name
+        except SkippedEndException as see:
+            print(see.txt)
+        return self.winner
 
     def is_final(self):
         self.is_win()
+        is_end = "NOT END"
         is_full = True
-        for i in range(0, 2):
-            for j in range(0, 2):
-                if self.field[i][j] == -1:
+        for x in range(0, 2):
+            for y in range(0, 2):
+                if self.field[x][y] == -1:
                     is_full = False
         if self.winner != "Nobody" or is_full:
-            print("END")
-        else:
-            print("Not END")
+            is_end = "END"
+        return is_end
 
+    def whose_turn(self):
+        if self.current_player:
+            return self.cross_name
+        return self.circle_name
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     ttt = TicTacToe("IVAN", "QWER")
     ttt.make_turn(1, 1)
     ttt.make_turn(2, 2)
-    ttt.is_final()
-    ttt.is_win()
     ttt.make_turn(1, 2)
-    ttt.make_turn(0, 0)
-    ttt.make_turn(1, 0)
-    ttt.is_final()
-    ttt.is_win()
+    print(ttt.is_final())
+    print(ttt.is_win())
+    ttt.make_turn(2, 1)
+    ttt.make_turn(1, 3)
+    ttt.make_turn(2, 3)
+    print(ttt.is_final())
+    print(ttt.is_win())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
